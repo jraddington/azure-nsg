f8c83c2a75c470253888f6f0205c598aeced9c24
Josh Addington <jraddington@hotmail.com>
2022-05-13 12:42:41 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,83 @@
+#Terraform code to create several NSGs
+
+#Create a Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+#Create an NSG
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+#Create Rule 1 - Defaut Deny Inbound must be present
+  security_rule {
+    name                       = "default_deny_inbound"
+    priority                   = 4096
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  #Create Rule 2 - Defaut Deny Outbound must be present
+  security_rule {
+    name                       = "default_deny_outbound"
+    priority                   = 4096
+    direction                  = "Outbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  #Create Rule 3 - Source and/or Destination
+  security_rule {
+    name                       = "source"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  #Create Rule 4 - Avoid Any Protocol
+  security_rule {
+    name                       = "any"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  #Create Rule 5 - No VirtualNetwork
+  security_rule {
+    name                       = "vnet"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
